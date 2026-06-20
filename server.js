afc0e990d423b5f0db089d309daa437a21288e3b
Motiv-Rewards <joshusernametaken@gmail.com>
2026-06-20 16:23:37 -0400

    Change static file serving directory in server.js

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ await db.write()
 const app = express()
 app.use(cors())
 app.use(express.json({ limit: '10mb' }))
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname)))
 
 function auth(req, res, next) {
   const token = req.headers.authorization?.split(' ')[1]
