2e601694f9dc138f40543a7f695a440b5131f4ee
Motiv-Rewards <joshusernametaken@gmail.com>
2026-06-20 16:19:15 -0400

    Serve static files from the 'public' directory

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ await db.write()
 const app = express()
 app.use(cors())
 app.use(express.json({ limit: '10mb' }))
-app.use(express.static(path.join(__dirname)))
+app.use(express.static(path.join(__dirname, 'public')))
 
 function auth(req, res, next) {
   const token = req.headers.authorization?.split(' ')[1]
